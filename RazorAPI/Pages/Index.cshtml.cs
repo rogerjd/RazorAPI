9fdeb31c0a106b91e990e0e11699cce53aa7384b
rjpd@netzero.com <rogerjd>
2019-03-29 22:39:06 -0400

    Add DI to startup

--- RazorAPI/Pages/Index.cshtml.cs
+++ RazorAPI/Pages/Index.cshtml.cs
@@ -9,9 +9,12 @@ namespace RazorAPI.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-
-        }
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+        public int Doors { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- RazorAPI/Startup.cs
+++ RazorAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using static RazorAPI.Services.CarService;
 
 namespace RazorAPI
 {
@@ -33,6 +34,8 @@ namespace RazorAPI
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMemoryCache();
+            services.AddTransient<ICarService, CarServive>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
